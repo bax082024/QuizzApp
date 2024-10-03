e31f668f5374930077e054cd9f662ba65f82ce59
bax082024 <bax082024@gmail.com>
2024-10-03 14:04:50 +0200

    added program.cs

--- Program.cs
+++ Program.cs
@@ -1,1 +1,17 @@
-﻿
+﻿using System;
+
+class Program
+{
+  static void Main(string[] args)
+  {
+    QuizService quizService = new QuizService();
+    QuizUI quizUI = new QuizUI(quizService);
+
+    Console.WriteLine("Welcome to the Quizz App!");
+    quizUI.StartQuiz();
+
+  }
+
+
+
+}
